ad5c4202663b63c5ddea7e70cc2dd2fb5d6854f9
AhmadSalihu <Ahmadsalihu419@gmail.com>
2021-11-28 15:06:18 +0100

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0ee4679725000d6db6367c3ff6325309'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e0fa9b91-bf37-41ee-bfe4-22322c4fac63'
+};
